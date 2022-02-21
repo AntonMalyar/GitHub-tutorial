66cebfa4cde0482b627231569b6a565a48d3510a
Anton Malyar <antn.mlr@gmail.com>
2022-02-21 11:30:58 +0200

    created method_1 into the ÐMain.lass and compiled it

--- src/javaClasses/Main.java
+++ src/javaClasses/Main.java
@@ -1,6 +1,11 @@
 package javaClasses;
 public class Main {
+    public void method_1(){
+        System.out.println("method_1");
+    }
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        Main main  = new Main();
+        main.method_1();
     }
 }
